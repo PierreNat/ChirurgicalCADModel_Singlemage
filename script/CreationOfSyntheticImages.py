a05b80d21d318cf77657c78eb05608a60aa1494c
PierreNat <pierre.cuony@students.unibe.ch>
2019-08-10 11:22:38 +0200

    rotation script 6 param render

--- script/CreationOfSyntheticImages.py
+++ script/CreationOfSyntheticImages.py
@@ -29,11 +29,11 @@ def main():
     print(vertices_1.shape)
     print(faces_1.shape)
 
-    file_name_extension = 'Translation_im3'
+    file_name_extension = 'Rotation_Translation_im3'
 
 
 
-    nb_im = 1
+    nb_im = 100
     #init and create renderer object
     R = np.array([np.radians(0), np.radians(0), np.radians(0)])  # angle in degree
     t = np.array([0, 0, 0])  # translation in meter
@@ -52,7 +52,7 @@ def main():
         beta = 0#uniform(0, 180)
         gamma = 0#uniform(0, 180)
         x = 0#uniform(-2, 2)
-        y = 1.5#uniform(-2, 2)
+        y = 0#uniform(-2, 2)
         z = 6#uniform(5, 10) #1000t was done with value between 7 and 10, Rot and trans between 5 10
         R = np.array([np.radians(alpha), np.radians(beta), np.radians(gamma)])  # angle in degree
         t = np.array([x, y, z])  # translation in meter
